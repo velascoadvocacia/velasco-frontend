71c52dd3903d5c6f4929ade19a865bbc65aa6d1d
Paulo Bueno <phf.bueno@gmail.com>
2026-08-21 18:12:26 -0300

    Adiciona dano moral por assédio moral

--- src/pages/RTComposerPage.tsx
+++ src/pages/RTComposerPage.tsx
@@ -76,6 +76,7 @@ type BlockId =
     | "adicional_periculosidade"
     | "periculosidade_tanque_suplementar"
     | "dano_moral_cobranca_abusiva_metas"
+    | "dano_moral_assedio_moral"
     | "baixa_ctps_tutela"
     | "rescisao_indireta_tutela_antecipada_verbas_incontroversas"
     | "tutela_urgencia_natureza_cautelar"
@@ -190,7 +191,8 @@ const BLOCKS_FROM_API: BlockId[] = [
   "adicional_insalubridade",
   "adicional_periculosidade",
   "periculosidade_tanque_suplementar",
-  "dano_moral_cobranca_abusiva_metas"
+  "dano_moral_cobranca_abusiva_metas",
+  "dano_moral_assedio_moral"
 ];
 
 const severanceChildBlockIds: BlockId[] = [
@@ -653,6 +655,7 @@ const blockDefinitions: BlockDefinition[] = [
   { id: "adicional_periculosidade", title: "Adicional de periculosidade", section: "Adicional de periculosidade" },
   { id: "periculosidade_tanque_suplementar", title: "Adicional de periculosidade. Tanque suplementar. Lei n.º 14.766/2023", section: "Adicional de periculosidade" },
   { id: "dano_moral_cobranca_abusiva_metas", title: "Dano moral pela cobrança abusiva de metas", section: "Danos morais" },
+  { id: "dano_moral_assedio_moral", title: "Dano moral por assédio moral", section: "Danos morais" },
 ];
 
 const defaultBlocks: BlockId[] = [
